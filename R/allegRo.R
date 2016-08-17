9e38479e23101b064b999e3c2a3367d6e0e6c8a6
Boudewijn Aasman <baasman@montefiore.org>
2016-08-17 08:53:44 -0400

    added startup message

---
+++ R/allegRo.R
@@ -0,0 +1,6 @@
+.packageName = "allegRo"
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to allegRo, the R package to interact with AllegroGraph. To start working with RDF data and AllegroGraph, install
+                        a free server at http://franz.com/agraph/downloads/")
+}
